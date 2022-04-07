69c78a800a3c0e45d591c057f446be3a5288c5ad
Dilara Gunasekera <Dilara8529@gmail.com>
2022-04-07 13:31:24 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner changeowed = new Scanner(System.in);
+        int centcount = 0;
+        float change;
+        do {
+            System.out.println("How much change is owed?: ");
+            change = changeowed.nextFloat();
+        }while (change <= 0.00);
+
+
+        int cents = Math.round(change * 100);
+
+
+        while (cents >= 25)
+        { centcount++;
+            cents -= 25;
+        }
+        while (cents >= 10)
+        { centcount++;
+            cents -= 10;
+        }
+        while (cents >= 5)
+        { centcount++;
+            cents -= 5;
+        }
+        while (cents >= 1)
+        { centcount++;
+            cents -= 1;
+        }
+
+
+        System.out.println( "Number of coins required: " + centcount);
+
+    }
+}
